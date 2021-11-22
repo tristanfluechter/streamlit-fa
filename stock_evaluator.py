aebe1fb2245ef9ef7a6202ba7ec7b02eaedeb229
Tristan Fluechter <tristan.fluechter@gmail.com>
2021-11-22 22:04:52 +0000

    Test

--- stock_evaluator.py
+++ stock_evaluator.py
@@ -288,7 +288,7 @@ def get_stock_data(stock_ticker, start_date, end_date):
 
 def error_message():
     st.header("ERROR: Could not execute module.")
-    st.image("/images/Error_Message.jpg")
+    st.image("images/Error_Message.jpg")
     st.write("An error has occurred. The reason is an invalid input in either the stock ticker or date sidebar widgets. Please ensure correct format (valid ticker and start/end dates).")
 
 def app():
