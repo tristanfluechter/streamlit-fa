72e34c3f898f3caa81702796a25e3f3ada5926fa
Tristan Fluechter <tristan.fluechter@gmail.com>
2021-11-22 22:06:30 +0000

    Error Handling

--- stock_evaluator.py
+++ stock_evaluator.py
@@ -287,10 +287,13 @@ def get_stock_data(stock_ticker, start_date, end_date):
     return stock_data
 
 def error_message():
-    st.header("ERROR: Could not execute module.")
+    st.write("""
+             # Error: Could not execute module.
+             An error has occurred. The reason is an invalid input in either the stock ticker or date sidebar widgets. 
+             Please ensure correct format (valid ticker and start/end dates).
+             """)
     st.image("images/Error_Message.jpg")
-    st.write("An error has occurred. The reason is an invalid input in either the stock ticker or date sidebar widgets. Please ensure correct format (valid ticker and start/end dates).")
-
+    
 def app():
     """
     Main streamlit app to deploy.
