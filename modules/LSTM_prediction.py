ac727dfff6fd6a4f60261c80e30975c953afaada
Tristan Fluechter <tristan.fluechter@gmail.com>
2021-11-25 11:59:55 +0000

    test

--- modules/LSTM_prediction.py
+++ modules/LSTM_prediction.py
@@ -189,7 +189,7 @@ def lstm_make_prediction(model, look_back, stockdata, close_data, close_data_noa
     
     st.plotly_chart(fig2, use_container_width=True)
     
-    lstm_pred = prediction_list["0"].iloc[-1]
+    lstm_pred = prediction_list.iloc[-1]
     
     return lstm_pred
 
